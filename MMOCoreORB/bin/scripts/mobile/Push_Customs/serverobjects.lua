403ee7b5c4c87c20012cf4f324ae003f8213195f
Push <leif.rice@rocketmail.com>
2024-01-25 17:38:36 -0700

    Tony's Memorial

--- MMOCoreORB/bin/scripts/mobile/Push_Customs/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/Push_Customs/serverobjects.lua
@@ -3,7 +3,14 @@ includeFile("Push_Customs/donator_droid.lua")
 includeFile("Push_Customs/sher_kar_boss.lua")
 includeFile("Push_Customs/sher_kar_karling.lua")
 includeFile("Push_Customs/sher_kar_praetorian.lua")
+includeFile("Push_Customs/teleport.lua")
 
 --Twin Sabers
 includeFile("Push_Customs/twin_sabers/emptat.lua")
 includeFile("Push_Customs/twin_sabers/darkspirit.lua")
+
+--Tony's Memorial
+includeFile("Push_Customs/tony_memorial.lua")
+includeFile("Push_Customs/male_ewok.lua")
+includeFile("Push_Customs/memorialguard.lua")
+includeFile("Push_Customs/memorialjedi.lua")
--- MMOCoreORB/bin/scripts/screenplays/Push_Customs/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/Push_Customs/screenplays.lua
@@ -5,7 +5,14 @@ includeFile("Push_Customs/tatooine_mos_eisley.lua")
 includeFile("Push_Customs/tatooine_mos_espa.lua")
 includeFile("Push_Customs/sher_kar.lua")
 includeFile("Push_Customs/sher_kar_objects.lua")
+--includeFile("Push_Customs/bloodfin_cantina.lua")
 
 --Twin Sabers
 includeFile("Push_Customs/twin_sabers/twin_sabers.lua")
 includeFile("Push_Customs/twin_sabers/twin_sabers_objects.lua")
+
+--DWS
+includeFile("Push_Customs/dws/dws_objects.lua")
+
+--Tony's Memorial
+includeFile("Push_Customs/memorials/TonyMemorial.lua")
