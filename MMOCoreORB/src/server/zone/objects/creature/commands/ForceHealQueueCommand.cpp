f1b81c2b1d4a209b99be2823c22060158dbd46a9
DevRogue <swguy270@gmail.com>
2024-02-20 20:39:19 -0600

    Jedi heals GCD reduced to 1s

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceHealQueueCommand.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceHealQueueCommand.cpp
@@ -11,7 +11,7 @@
 #include "server/zone/objects/player/FactionStatus.h"
 
 ForceHealQueueCommand::ForceHealQueueCommand(const String& name, ZoneProcessServer* server) : JediQueueCommand(name, server) {
-	speed = 3;
+	speed = 1;
 	allowedTarget = TARGET_AUTO;
 
 	forceCost = 0;
