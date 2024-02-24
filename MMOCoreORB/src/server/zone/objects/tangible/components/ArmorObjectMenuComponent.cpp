ae1fbef08cb1b44054528ef813dc81f5ccb76c49
Push <leif.rice@rocketmail.com>
2024-02-23 17:24:47 -0700

    Armor/Clothing Color Update
    Added funtion to be able to color primary and secondary colors on armor
    and clothing.

--- MMOCoreORB/src/server/zone/objects/tangible/components/ArmorObjectMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/ArmorObjectMenuComponent.cpp
@@ -11,12 +11,17 @@
 #include "server/zone/objects/building/BuildingObject.h"
 #include "server/zone/objects/player/sui/colorbox/SuiColorBox.h"
 #include "ArmorObjectMenuComponent.h"
+#include "server/zone/objects/scene/components/ObjectMenuComponent.h"
 #include "server/zone/packets/object/ObjectMenuResponse.h"
+#include "server/zone/managers/objectcontroller/ObjectController.h"
+#include "server/zone/objects/player/sui/SuiCallback.h"
 #include "server/zone/objects/player/sui/callbacks/ColorArmorSuiCallback.h"
+#include "server/zone/Zone.h"
 #include "server/zone/ZoneServer.h"
 #include "templates/customization/AssetCustomizationManagerTemplate.h"
 
 void ArmorObjectMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const {
+	
 	if (!sceneObject->isWearableObject())
 		return;
 
@@ -31,20 +36,25 @@ void ArmorObjectMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, 
 			if (!buio->isOnAdminList(player))
 				return;
 		}
-	} else {
+	} 
+	else 
+	{
 		if (!sceneObject->isASubChildOf(player))
 			return;
 	}
 
 	String text = "Color Change";
-	menuResponse->addRadialMenuItem(81, 3, text);
+	menuResponse->addRadialMenuItem(80, 1, text);
+	menuResponse->addRadialMenuItemToRadialID(80, 81, 3, "Primary");
+	menuResponse->addRadialMenuItemToRadialID(80, 82, 3, "Secondary");
 
 	WearableObjectMenuComponent::fillObjectMenuResponse(sceneObject, menuResponse, player);
 }
 
 int ArmorObjectMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* player, byte selectedID) const {
 
-	if (selectedID == 81) {
+	if (selectedID == 81 || selectedID == 82) {
+
 		ManagedReference<SceneObject*> parent = sceneObject->getParent().get();
 
 		if (parent == nullptr)
@@ -64,7 +74,9 @@ int ArmorObjectMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, C
 				if (!buio->isOnAdminList(player))
 					return 0;
 			}
-		} else {
+		} 
+		else 
+		{
 			if (!sceneObject->isASubChildOf(player))
 				return 0;
 		}
@@ -72,19 +84,28 @@ int ArmorObjectMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, C
 		ZoneServer* server = player->getZoneServer();
 
 		if (server != nullptr) {
+
+			int index = selectedID == 81 ? 0 : 1;
+
 			// The color index.
 			String appearanceFilename = sceneObject->getObjectTemplate()->getAppearanceFilename();
 			VectorMap<String, Reference<CustomizationVariable*> > variables;
 			AssetCustomizationManagerTemplate::instance()->getCustomizationVariables(appearanceFilename.hashCode(), variables, false);
 
+			for(int i = 0; i < variables.size(); i++)
+			{
+	
+				String varkey = variables.elementAt(i).getKey();
+				if (varkey.contains("color")){
+
 			// The Sui Box.
 			ManagedReference<SuiColorBox*> cbox = new SuiColorBox(player, SuiWindowType::COLOR_ARMOR);
 			cbox->setCallback(new ColorArmorSuiCallback(server));
-			cbox->setColorPalette(variables.elementAt(1).getKey()); // First one seems to be the frame of it? Skip to 2nd.
+			cbox->setColorPalette(variables.elementAt(index).getKey()); // First one seems to be the frame of it? Skip to 2nd.
 			cbox->setUsingObject(sceneObject);
 
-			int skillMod = 255; //player->getSkillMod("armor_customization");
-
+			int skillMod = 500; //player->getSkillMod("armor_customization");
+			
 			/*
 			if (skillMod < 64)
 				skillMod = 64;
@@ -101,9 +122,11 @@ int ArmorObjectMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, C
 				ghost->addSuiBox(cbox);
 				player->sendMessage(cbox->generateMessage());
 			}
+			}
 		}
 
 	}
+}
 
 	return WearableObjectMenuComponent::handleObjectMenuSelect(sceneObject, player, selectedID);
 }
