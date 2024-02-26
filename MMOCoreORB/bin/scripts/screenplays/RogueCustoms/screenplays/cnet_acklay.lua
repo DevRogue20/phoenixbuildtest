4371476715a680da82aedcb729957134994724a8
DevRogue <swguy270@gmail.com>
2024-02-25 19:04:10 -0600

    cnet acklay forgot lol

---
+++ MMOCoreORB/bin/scripts/screenplays/RogueCustoms/screenplays/cnet_acklay.lua
@@ -0,0 +1,25 @@
+cnetAcklay = ScreenPlay:new{
+    numberOfActs = 1,
+
+    screenplayName = "cnetAcklay"
+
+}
+
+registerScreenPlay("cnetAcklay", true)
+
+function cnetAcklay
+:start()
+    if (isZoneEnabled("corellia")) then
+      self:spawnMobiles()
+    end
+end
+
+function cnetAcklay
+:spawnMobiles()
+
+    -- cornerhall1 CELL ID [9995375]
+    spawnMobile("corellia", "acklay", 300, 60.9, 28.0, -4615.5, -76, 0)
+
+end
+
+
--- MMOCoreORB/bin/scripts/screenplays/RogueCustoms/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/RogueCustoms/screenplays/screenplays.lua
@@ -1,4 +1,5 @@
 -- Rogue's Custom Screenplays
 includeFile("RogueCustoms/screenplays/tat_mando_bunker_test.lua")
+includeFile("RogueCustoms/screenplays/cnet_acklay.lua")
 
 
